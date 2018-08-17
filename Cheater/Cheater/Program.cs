434d91f7c40fc1a211c04e5e70850165e3bbc9c0
Oksana <oksana.barinova12@outlook.com>
2018-08-17 23:27:57 +0300

    add writeline in main

---
+++ Cheater/Cheater/Program.cs
@@ -0,0 +1,31 @@
+﻿using Cheater.Players;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cheater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberOfPlayers game = new NumberOfPlayers();
+            int players = game.GetPlNumber();
+
+            GuessWeight gv = new GuessWeight();
+            int weight = gv.Weight(40, 140);
+
+            PlayerInit pi = new PlayerInit(40, 140);
+            BasePlayer[] array = pi.GetPlayers(players);
+
+
+            WeightCompare weightCompare = new WeightCompare(weight, array,100);
+            weightCompare.RunComparator();
+            Console.WriteLine("change");
+
+            Console.ReadKey();
+        }
+    }
+}
